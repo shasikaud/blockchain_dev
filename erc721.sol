f8210db65a2a149f886e36d08086b88e82f083d2
Shasika Udayanga <shasikaudayanga@gmail.com>
2021-07-19 09:41:38 +0530

    Create erc721.sol

---
+++ erc721.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/presets/ERC721PresetMinterPauserAutoId.sol";
+
+//token info -> hosted in github -> accessd as a json obj via json-decoder server
+//https://my-json-server.typicode.com/shasikaud/test_rest/tokens
