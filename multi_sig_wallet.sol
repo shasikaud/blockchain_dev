061310d6f2df03ab7a930867582d443f8abf2847
Shasika Udayanga <shasikaudayanga@gmail.com>
2021-08-13 15:46:33 +0530

    added modifiers

--- multi_sig_wallet.sol
+++ multi_sig_wallet.sol
@@ -1,3 +1,4 @@
+// 2021-08-13 02:00 AM (UTC
 pragma solidity ^0.8.2;
 
 contract MultiSigWallet {
@@ -22,6 +23,7 @@ contract MultiSigWallet {
     */
     
     address[] public owners; //array to store the owners 
+    mapping(address => bool) public isOwner;   //whether address corresponds to a unique owner 
     uint public numConfirmationsRequired;
     
     struct Transaction {
@@ -41,15 +43,60 @@ contract MultiSigWallet {
         require(_owners.length > 0, "Owners required");
         require(_numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length, "Invalid number of required confirmations");
         
+        //initializing the owners
         for (uint i=0; i<_owners.length; i++){
             address owner = _owner[i];
             require(owner != address(0), "Invalid Owner");
+            require(!isOwner[owner], "Owner not unique");
+            isOwner[owner] = true;
+            owners.push(owner);
         }
+        
+        numConfirmationsRequired = _numConfirmationsRequired;
+        
     }
     
-    function submitTransaction() {}
+    modifier onlyOwner() {
+        require (isOwner[msg.sender], "Not Owner");
+        _;
+    }
     
-    function confirmTransaction() {}
+    function submitTransaction(address _to, uint _value, bytes memory _data) public onlyOwner {
+        uint txIndex = transactions.length; //transactions counter
+        transactions.push(Transaction({
+            to: _to,
+            value: _value,
+            data: _data,
+            executed: false,
+            numConfirmations: 0
+        }));
+        
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+        
+    }
+    
+    modifier txExists(uint _txIndex){
+        require (_txIndex < transactions.length, "Tx does not exists");
+        _;
+    }
+    
+     modifier notExecuted(uint _txIndex){
+        require (!transactions[_txIndex].executed, "Tx already executed");
+        _;
+    }
+    
+     modifier notConfirmed(uint _txIndex){
+        require (!transactions[_txIndex].isConfirmed[msg.sender], "Tx already confirmed);
+        _;
+    }
+    
+    function confirmTransaction(uint _txIndex) public onlyOwner txExists(_txIndex) notExecuted(_txIndex) notConfirmed(_txIndex) {
+        Transaction storage transaction = transactions[_txIndex]; //retrieve and store the transaction in var transaction locally
+        transaction.isConfirmed[msg.sender] = true;
+        transaction.numConfirmations += 1;
+        
+        emit ConfirmTransaction(msg.sender, _txIndex);
+    }
     
     function executeTransaction() {}
     
