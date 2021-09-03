2be505b57d9a40412d6479dfa18b779335c4ef08
Shasika Udayanga <shasikaudayanga@gmail.com>
2021-09-03 20:06:05 +0530

    started on minting function

--- Blockchain Application 02/miniting-application/src/contracts/DappTokenCrowdsale.sol
+++ Blockchain Application 02/miniting-application/src/contracts/DappTokenCrowdsale.sol
@@ -1,8 +1,9 @@
 pragma solidity >=0.4.23;
 
 import "openzeppelin-solidity/contracts/crowdsale/Crowdsale.sol";
+import "openzeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol";
 
-contract DappTokenCrowdsale is Crowdsale {
+contract DappTokenCrowdsale is Crowdsale, MintedCrowdsale {
 
     constructor (uint256 _rate, address _walllet, ERC20 _token) 
         Crowdsale(_rate, _walllet, _token)
--- Blockchain Application 02/miniting-application/test/DappTokenCrowdsale.js
+++ Blockchain Application 02/miniting-application/test/DappTokenCrowdsale.js
@@ -1,4 +1,7 @@
+import ether from './helpers/ether';
+
 const { assert } = require('chai');
+const BigNumber = web3.BigNumber;
 
 const DappToken = artifacts.require("./DappToken.sol");
 const DappTokenCrowdsale = artifacts.require("./DappTokenCrowdsale.sol");
@@ -8,39 +11,41 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('DappTokenCrowdsale', ([_, wallet]) => {    //these corresponds to the first 3 acs in ganache
-    
-    let dappToken
-    let dappTokenCrowdsale
+contract('DappTokenCrowdsale', ([_, wallet, investor1, investor2]) => {    //these corresponds to the first 3 acs in ganache
 
     beforeEach(async function () {
 
         //deploy token 
-        dappToken = await DappToken.new("M2 TOKEN", "M2C", 18);   
+        this.token = await DappToken.new("M2 TOKEN", "M2C", 18);   
         
         //crwodsale configuration
         this.rate = 500;  //500 tokens per 1 ETH
         this.wallet = wallet;
-        this.address = await dappToken.address;
-        dappTokenCrowdsale = await DappTokenCrowdsale.new(this.rate, this.wallet, this.address);
-    })
+        this.crowdsale = await DappTokenCrowdsale.new(this.rate, this.wallet, this.token.address);
+    });
 
 
-    describe("deployment", async () => {
-        it ('deploys successfully', async () => {
-            // const address = await dappTokenCrowdsale.address
-            // assert.notEqual(address, 0x0)
-            // assert.notEqual(address, '')
-            // assert.notEqual(address, null)
-            // assert.notEqual(address, undefined)
-        })
+    describe("crowdsale deployment", function () {
+        it ('tracks the token', async function () {
+            const token = await this.crowdsale.token()
+            //token.should.equal(this.token.address)
+            assert.equal(token, this.token.address)
+        });
 
-        // it ('intialisation is correct', async () => {
-        //     const name = await dappTokenCrowdsale.name()
-        //     assert.equal(name, 'Dapp Token')
-        // })
+        it ('tracks the rate', async function () {
+            const rate = await this.crowdsale.rate()
+            //rate.should.be.a.bignumber.equal(this.rate);
+            assert.equal(rate, this.rate)
+        });
+    });
 
-    })
+    describe("accepting payments", function () {
+        it ('accept payments', async function () {
+            const value = ether(1);
+            await this.crowdsale.sendTransaction({ value: value, from: investor1})
+        });
+
+    });
 
 
-})
+});
