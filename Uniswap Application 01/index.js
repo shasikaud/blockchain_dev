2c60dba8312ad17eaa206fe39bafdff0f3769859
Shasika Udayanga <shasikaudayanga@gmail.com>
2021-09-11 09:11:06 +0530

    modified index.js

---
+++ Uniswap Application 01/index.js
@@ -0,0 +1,14 @@
+const { ChainId, Fetcher, WETH, Route} = require('@uniswap/sdk');
+
+const chainId = ChainId.MAINNET;
+const tokenAddress = '0x6b175474e89094c44da98b954eedeac495271d0f'; //DAI TOKEN
+
+const init = async () => {
+    const dai = await Fetcher.fetchTokenData(chainId, tokenAddress);
+    const weth = WETH[chainId];
+    const pair = await Fetcher.fetchPairData(dai, weth);
+    const route = new Route([pair],weth);
+    console.log(route.midPrice.invert().toSignificant(6));
+}
+
+init();
